f46f5be7116ff0d6eb98127cb11568b69951474c
Sandeep Kumar <sandeepkumar.er21@gmail.com>
2024-06-25 12:49:57 -0700

    Update Description Run_1

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "intersight" {
 
 resource "intersight_ntp_policy" "ntp_policy" {
   name        = "tf_cloud_demo_1"
-  description = "Policy Created using Terraform Cloud TF_Run_0"
+  description = "Policy Created using Terraform Cloud TF_Run_1"
   enabled     = true
   ntp_servers = ["1.1.1.2", "2.2.2.5"]
   timezone    = "America/Los_Angeles"
